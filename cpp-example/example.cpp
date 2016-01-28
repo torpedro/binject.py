43850d9071a16fe49b7a8848d7f6dcf8f4ea758b
Pedro Flemming <pedro.flemming@valsight.com>
2016-01-28 23:43:04 +0100

    extended example.cpp

--- cpp-example/example.cpp
+++ cpp-example/example.cpp
@@ -1,21 +1,40 @@
 
 #include <stdio.h>
+#include <string>
+#include <cstdlib>
 
+/**
+ * sums up all numbers from 1 to n
+ */
+long sum(int n) {
+	long result = 0;
+	for (int i = 1; i <= n; ++i) {
+		result += i; // <inject-skip>
+	}
+
+	if (n % 2 == 1) {
+		printf("fault\n"); // <inject-fault>
+	}
+	return result;
+}
 
 
 
 
 int main(int argc, char const *argv[]) {
 
-
 	int x = 0;
 	const char* y = "abcd";
 	double z = 2312;
 
 	printf("%d\n", x);
-	printf("%s\n", y); // <inject-fault>
-	printf("%.2f\n", z);
+	printf("%s\n", y); 
+	printf("%.2f\n", z); 
 
+	for (int i = 1; i < argc; ++i) {
+		int input = atoi(argv[i]);
+		printf("Sum of %d: %ld\n", input, sum(input));
+	}
 
 	return 0;
 }
